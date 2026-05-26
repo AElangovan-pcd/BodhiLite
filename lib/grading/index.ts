109747a494f9fa0da2e10818a857ba57233790f2
A. Elangovan <aelangovan.pcd@gmail.com>
2026-05-25 21:28:56 -0700

    feat(grading): add computeAttemptSummary + barrel exports

--- lib/grading/index.ts
+++ lib/grading/index.ts
@@ -1,2 +1,6 @@
 export { evaluate, EvalError } from './formula';
 export { molarMass } from './chem-data/molar-mass';
+export * from './snapshot';
+export * from './response';
+export * from './grade';
+export * from './summary';
